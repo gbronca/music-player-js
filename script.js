f85509381a0db0adb275262ff751877371c17d1a
gbronca <42703789+gbronca@users.noreply.github.com>
2020-08-21 21:36:13 +0100

    scroll between songs using prev and next buttons

--- script.js
+++ script.js
@@ -1,7 +1,34 @@
+const image = document.querySelector('img');
 const music = document.querySelector('audio');
+const artist = document.getElementById('artist');
+const title = document.getElementById('title');
 const prevBtn = document.getElementById('prev');
 const playBtn = document.getElementById('play');
 const nextBtn = document.getElementById('next');
+
+// Music
+const songs = [
+	{
+		name: 'jacinto-1',
+		displayName: 'Electric Machine',
+		artist: 'Jacinto Design',
+	},
+	{
+		name: 'jacinto-2',
+		displayName: 'Seven Nation Army',
+		artist: 'Jacinto Design',
+	},
+	{
+		name: 'jacinto-3',
+		displayName: 'Electric Machine',
+		artist: 'Jacinto Design',
+	},
+	{
+		name: 'metric-1',
+		displayName: 'Front Row',
+		artist: 'Metric/Jacinto Design',
+	},
+]
 
 // check if is playing
 let isPlaying = false;
@@ -22,5 +49,45 @@ function pauseSong() {
 	music.pause();
 }
 
+// Update DOM
+function loadSong(song) {
+	title.textContent = song.displayName;
+	artist.textContent = song.artist;
+	music.src = `music/${song.name}.mp3`;
+	image.src = `img/${song.name}.jpg`;
+}
+
+// Current Song
+let songIndex = 0;
+
+// Previous Song
+function prevSong() {
+	songIndex--;
+	if (songIndex < 0) {
+		songIndex = songs.length - 1;
+	}
+	loadSong(songs[songIndex]);
+	playSong();
+}
+
+// Next Song
+function nextSong() {
+	songIndex++;
+	if (songIndex > songs.length - 1) {
+		songIndex = 0;
+	}
+	loadSong(songs[songIndex]);
+	playSong();
+}
+
+// On Load - select first song
+loadSong(songs[songIndex]);
+
+
+
 // Play or Pause Event Listener
 playBtn.addEventListener('click', () => (isPlaying ? pauseSong() : playSong()));
+
+// Event Listeners
+prevBtn.addEventListener('click', prevSong);
+nextBtn.addEventListener('click', nextSong);
